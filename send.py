0351516cc2f717602e14b4b2e43491685d64d65b
vudala <vudalae@gmail.com>
2025-01-30 22:31:29 -0300

    fix

--- send.py
+++ send.py
@@ -3,8 +3,8 @@ from scapy.layers.l2 import Ether
 
 from lineorder import LINEORDER_TYPE, LineOrder
 
-interface = 'eth0'
-destiny = "02:42:ac:14:00:02"
+interface = 'veth0'
+destiny = '2a:37:ae:62:92:8'
 
 bind_layers(Ether, LineOrder, type=LINEORDER_TYPE)
 
@@ -20,33 +20,27 @@ ether_frame = Ether(dst=destiny)
 packets = []
 for l in lines[:10]:
   payload = LineOrder(
-    lo_orderkey = l[0],
-    lo_linenumber = l[1],
-    lo_custkey = l[2],
-    lo_partkey = l[3],
-    lo_suppkey = l[4], 
+    lo_orderkey = int(l[0]),
+    lo_linenumber = int(l[1]),
+    lo_custkey = int(l[2]),
+    lo_partkey = int(l[3]),
+    lo_suppkey = int(l[4]), 
     lo_orderdate = l[5],
     lo_orderpriority = l[6],
     lo_shippriority = l[7],
-    lo_quantity = l[8],
-    lo_extendedprice = l[9],
-    lo_ordtotalprice = l[10],
-    lo_discount = l[11],
-    lo_revenue = l[12],
-    lo_supplycost = l[13],
-    lo_tax = l[14],
+    lo_quantity = int(l[8]),
+    lo_extendedprice = int(l[9]),
+    lo_ordtotalprice = int(l[10]),
+    lo_discount = int(l[11]),
+    lo_revenue = int(l[12]),
+    lo_supplycost = int(l[13]),
+    lo_tax = int(l[14]),
     lo_commitdate = l[15],
     lo_shipmode = l[16],  
   )
 
   packt = ether_frame / payload
 
-  packets.append(packt)
+  packt.show()
 
-packets[0].show()
-
-for p in packets:
-  sendp(p, iface=interface)
-
-# Send the pa = cket
-# sendp(packt, iface=interface)
+  sendp(packt, iface=interface)
